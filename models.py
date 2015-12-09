2591b351a80c9d385547a8ce5aadcf3dafe27a33
Joel Pena <jpena60@gmail.com>
2015-12-09 00:52:34 +0000

    Set up application

--- models.py
+++ models.py
@@ -3,3 +3,6 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Information(models.Model):
+	person_name = models.CharField(max_length = 50)
+	person_email = models.CharField(max_length = 50)
